46739a96753fe5abdadecf3d71129f4d8fb993fa
banyaadski <89918249+banyaadski@users.noreply.github.com>
2022-05-01 17:29:52 +0300

    Add files via upload

---
+++ HTML/js/jquery.js
@@ -0,0 +1,8 @@
+let username = prompt("Как зовут тебя, ситх-ученик? ", "Назови же своё имя");
+function Name(){
+    console.log('Импрерия приветствует тебя ' + username);
+}
+Name();   
+   
+    
+    
